0fe1fe4addec884614ba463597d5ebb4ebd4c091
Olivier Mengué <dolmen@cpan.org>
2016-12-11 05:28:38 +0100

    Contextual default install directory
    Either install into ~/.fonts/Go or ~/.local/share/fonts/Go if the parent
    dir exists.

--- get-go-fonts.sh
+++ get-go-fonts.sh
@@ -7,7 +7,19 @@ set -o errexit
 
 [[ -x /usr/bin/fc-cache ]] || { echo "Linux only :(" >&2 ; exit 1; }
 
-dir="${1:-~/.fonts/Go}"
+dir="$1"
+if [[ -z "$1" ]]; then
+	for d in ~/.fonts ~/.local/share/fonts
+	do
+		if [[ -e "$d" ]]; then
+			dir="$d/Go"
+			break
+		fi
+	done
+	dir="${dir:-~/.fonts/Go}"
+fi
+
+echo "Installing into $dir..."
 [[ -d "$dir" ]] || mkdir -p "$dir"
 cd "$dir"
 
