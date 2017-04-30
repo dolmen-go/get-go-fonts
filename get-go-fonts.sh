ae434d66f2b3d8a5ef9b0ea7abdf7c781ad4cfa3
Olivier Mengué <dolmen@cpan.org>
2017-04-30 23:11:13 +0200

    Report versions change after install/upgrade
    Thanks to @nigeltao and Khaled Hosny for the tips on extracting the
    version number from the .ttf files
    
    https://github.com/golang/image/commit/f03a046406d4d7fbfd4ed29f554da8f6114049fc#commitcomment-21898082

--- get-go-fonts.sh
+++ get-go-fonts.sh
@@ -23,8 +23,25 @@ echo "Installing into $dir..."
 [[ -d "$dir" ]] || mkdir -p "$dir"
 cd "$dir"
 
+function ttf_version
+{
+	[[ -e "$1" ]] || return 1
+	fc-query -f '%{fontversion}\n' "$1" | perl -E 'printf "%.3f", <>/65536.0'
+}
+
+if [[ -f "Go-Mono.ttf" ]]; then
+	old_version=$(ttf_version Go-Mono.ttf)
+fi
+
 curl https://go.googlesource.com/image/+archive/master/font/gofont/ttfs.tar.gz | tar xvzf -
 
 ls -la "$dir"
 
 fc-cache -fv
+
+new_version=$(ttf_version Go-Mono.ttf)
+if [[ -z "$old_version" ]]; then
+	echo "Go Fonts $new_version installed."
+elif [[ "$new_version" != "$old_version" ]]; then
+	echo "Go Fonts upgrade: $old_version -> $new_version"
+fi
